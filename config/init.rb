c226d3717ac9b751c1b339624eda47cc32f0bce1
Stephen Eley <sfeley@gmail.com>
2008-12-25 19:15:24 -0500

    Initial checkin

---
+++ config/init.rb
@@ -0,0 +1,24 @@
+# Go to http://wiki.merbivore.com/pages/init-rb
+ 
+require 'config/dependencies.rb'
+ 
+use_orm :datamapper
+use_test :rspec
+use_template_engine :haml
+ 
+Merb::Config.use do |c|
+  c[:use_mutex] = false
+  c[:session_store] = 'cookie'  # can also be 'memory', 'memcache', 'container', 'datamapper
+  
+  # cookie session store configuration
+  c[:session_secret_key]  = '5f08fdf820b2e883898a5c5186b4af1a7d00d006'  # required for cookie session store
+  c[:session_id_key] = '_malcontent_session_id' # cookie session id key, defaults to "_session_id"
+end
+ 
+Merb::BootLoader.before_app_loads do
+  # This will get executed after dependencies have been loaded but before your app's classes have loaded.
+end
+ 
+Merb::BootLoader.after_app_loads do
+  # This will get executed after your app's classes have been loaded.
+end
